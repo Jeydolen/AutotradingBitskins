478b3a74a73352231acedb1dfd64af1568739df2
Jeydolen <vandervoortadrien@gmail.com>
2020-06-15 14:24:11 +0200

    Fais pas chier

--- src/bb_fetcher.js
+++ src/bb_fetcher.js
@@ -24,6 +24,9 @@ const BITSKINS_FETCHER_SINGLETON    = "BITSKINS_FETCHER_SINGLETON";
 const PAGE_INDEX_START = 2; //----------------------------------------
 //______________________________________________________________________
 
+
+var a = a;
+
 /*
  /$$$$$$$  /$$   /$$              /$$       /$$                     /$$$$$$$$          /$$               /$$                          
 | $$__  $$|__/  | $$             | $$      |__/                    | $$_____/         | $$              | $$                          
@@ -40,6 +43,7 @@ class BitskinsFetcher
     static Instances = new Map();
     static result   = null;
     static Singleton = BitskinsFetcher.GetSingleton();
+    
     
     constructor (name)
     {
