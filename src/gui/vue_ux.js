b882dadf7ce1406fea5fd471c4b668fd8873a465
Jeydolen <vandervoortadrien@gmail.com>
2020-07-09 16:26:37 +0200

    v0.0.8.3.7

--- src/gui/vue_ux.js
+++ src/gui/vue_ux.js
@@ -4,9 +4,14 @@ if (! window.rekwire)       window[rekwire] = rekwire;
 
 // https://fr.vuejs.org/v2/guide/index.html
 
-var app = new Vue({
+var app = new Vue
+(
+  {
     el: '#app',
-    data: {
-      message: 'Hello Vue !, Coucouille'
+    data: 
+    {
+      message: 'Hello Vue !, Coucouille',
+      displayed: false
     }
-  })
+  }
+)
