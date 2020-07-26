e956e60b4502557caeeee08323d26ada3c304672
Jeydolen <vandervoortadrien@gmail.com>
2020-07-26 16:16:01 +0200

    v0.0.8.4.9 Checkbox implementation

--- data/sql/query/PASTOUCHERMICHEL.sql
+++ data/sql/query/PASTOUCHERMICHEL.sql
@@ -1,17 +1,17 @@
 SELECT * FROM
 	( 	
-		SELECT name as A_name, price as A_price, skin as A_skin, market_name as A_market_name FROM bitskins_csgo_prod.skin_sell_order WHERE skin_sell_order.skin IN
+		SELECT name as A_name, price as A_price, skin as A_skin, market_name as A_market_name FROM bitskins_csgo_prod.skin_sell_order 
+		WHERE item_state = 4 AND has_StatTrak = 1  AND skin_sell_order.skin IN
 		(	
-			SELECT id FROM bitskins_csgo_prod.skin WHERE skin_set = 5 AND skin_rarity = 4 AND item_state = 4 AND has_StatTrak = 1
+			SELECT id FROM bitskins_csgo_prod.skin WHERE skin_set = 5 AND skin_rarity = 4 
         )
 	) SQ_A
 INNER JOIN
 	( 	
-		SELECT name as B_name, price as B_price, skin as B_skin, market_name as B_market_name FROM bitskins_csgo_prod.skin_sell_order WHERE skin_sell_order.skin IN
+		SELECT name as B_name, price as B_price, skin as B_skin, market_name as B_market_name FROM bitskins_csgo_prod.skin_sell_order 
+		WHERE item_state = 4 AND has_StatTrak = 1  AND skin_sell_order.skin IN
 		(	
-			SELECT id FROM bitskins_csgo_prod.skin WHERE skin_set = 5 AND skin_rarity = 5 AND item_state = 4 AND has_StatTrak = 1
+			SELECT id FROM bitskins_csgo_prod.skin WHERE skin_set = 5 AND skin_rarity = 5
         )
 	) SQ_B
 ON  ((A_price * 1.00) * 10.00) < (B_price * 1.00);
-
-
--- src/microservices/api/query.service.js
+++ src/microservices/api/query.service.js
@@ -59,6 +59,96 @@ module.exports =
 
         async profit (ctx)
         {
+            var skin_set    = ctx.params.skinset    != undefined ? ctx.params.skinset   : 5;
+            var rarity      = ctx.params.rarity     != undefined ? ctx.params.rarity    : 4;
+            var state       = ctx.params.state      != undefined ? ctx.params.state     : 4;
+            var stattrak    = ctx.params.stattrak   != undefined ? ctx.params.stattrak  : 1;
+            var output = "<ol>";
+            var selectSellOrder_subquery =  null;
+            var selectSkin_subquery =       null;
+            var table_count = 0;
+            var previous_sq1_name = null;
+            var first_select = true;
+
+
+            const logResult = (rows) =>
+            {
+               rows.map
+                    ( row => 
+                        {   console.log(row) ;
+
+                            var sq1_name = row.SQ1_name;
+
+                            if (sq1_name != previous_sq1_name)
+                            {
+                                if (! first_select)
+                                {
+                                    output += "</select>";
+                                    output += "</li>";
+                                }
+
+                                output += "<li>";
+                                output += "<select name =" + sq1_name + ">";
+                                first_select = false;
+                            }
+                            output += "<option value = '" + row.SQ1_market_name + "'>" + sq1_name + ": " + row.SQ1_market_name + "</option>";
+                        
+                            previous_sq1_name = sq1_name;
+                        } 
+                    );
+            }; // logResult()
+
+            
+            const selectTradeUp = (subquery_1, subquery_2) =>
+            {
+                return  knex.select().from  (subquery_1)
+                            .innerJoin      (subquery_2)// , knex.raw('(SQ1_price * 10.00) < (SQ2_price * 1.00)'));
+            }; // selectTradeUp()
+
+
+            const selectSellOrder = (subquery, tmp_table) => 
+            {
+                return  knex.select('name as ' + tmp_table +'_name', 'price as ' + tmp_table +'_price', 'skin', 'market_name as ' + tmp_table +'_market_name').from('skin_sell_order')
+                     .where(  {'item_state' : state, 'has_StatTrak': stattrak } )
+                     .where( (builder) => builder.whereIn( 'skin_sell_order.skin', subquery ) ).as( tmp_table ) ;
+            }; // selectSellOrder
+
+
+            const selectSkin  = () => 
+            {
+                return query = knex.select('id').from('skin').where(
+                    {   'skin_set' : skin_set, 
+                        'skin_rarity': rarity++,
+                        //'item_state': state,
+                        //'has_StatTrak': stattrak             
+                    }
+                );
+            }; // selectSkin
+       
+            //--------------------------------------------------------
+            //await selectSkin().then( (rows) => logResult(rows) );
+            
+            //await selectSellOrder(selectSkin_subquery).then( (rows) => logResult(rows) );
+
+            selectSkin_subquery_1       = selectSkin();
+            selectSkin_subquery_2       = selectSkin();
+            selectSellOrder_subquery_1  = selectSellOrder( selectSkin_subquery_1, 'SQ1'  );
+            selectSellOrder_subquery_2  = selectSellOrder( selectSkin_subquery_2, 'SQ2'  );
+
+            await selectTradeUp(selectSellOrder_subquery_1, selectSellOrder_subquery_2).then( (rows) => logResult(rows) );
+
+
+            // https://developer.mozilla.org/fr/docs/Web/HTTP/Basics_of_HTTP/MIME_types
+            ctx.meta.$responseType = "text/html ; charset=utf-8";
+
+            output += "</ol>";
+            return output;
+        }, // 'profit' action
+
+
+
+        async profit_1 (ctx)
+        {
             var skin_set    = ctx.params.skinset    != undefined ? ctx.params.skinset   : 4;
             var rarity      = ctx.params.rarity     != undefined ? ctx.params.rarity    : 4;
             var state       = ctx.params.state      != undefined ? ctx.params.state     : 1;
@@ -72,7 +162,7 @@ module.exports =
                rows.map
                     ( row => 
                         { console.log(row) ;
-                        output += "<p>" + JSON.stringify(row) + "</p>";  
+                        output += "<li>" + JSON.stringify(row) + "</li>";  
                         } 
                     );
             }; // logResult()
@@ -123,7 +213,7 @@ module.exports =
             ctx.meta.$responseType = "text/html ; charset=utf-8";
 
             return output;
-        } // 'profit' action
+        } // 'profit_1' action
 
     } // query service actions
 };
